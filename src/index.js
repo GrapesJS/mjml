c68643d11992db106034520b6197251fcfc1eab6
Artur Arseniev <artur.catch@hotmail.it>
2017-11-09 13:22:05 +0100

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,89 @@
+export default grapesjs.plugins.add('gjs-mjml', (editor, opts = {}) => {
+  let c = opts;
+
+  let defaults = {
+    // Code viewer theme
+    codeViewerTheme: 'hopscotch',
+
+    // Import placeholder MJML
+    importPlaceholder: '',
+
+    // Title for the import modal
+    modalTitleImport: 'Import MJML',
+
+    // Test for the import button
+    modalBtnImport: 'Import',
+
+    // Description for the import modal
+    modalLabelImport: '',
+
+    // Title for the export modal
+    modalTitleExport: 'Export MJML',
+
+    // Description for the export modal
+    modalLabelExport: '',
+
+    // Overwrite default export command
+    overwriteExport: 1,
+
+    // String before the MJML in export code
+    preMjml: '<mjml><mj-body>',
+
+    // String after the MJML in export code
+    postMjml: '</mj-body></mjml>',
+
+    // Export 'mjml', 'html' or both (leave empty) TODO
+    exportOnly: '',
+
+    // Clean all previous blocks if true
+    resetBlocks: 1,
+
+    // Clean all previous devices and set a new one for mobile
+    resetDevices: 1,
+
+    // Reset the Style Manager and add new properties for MJML
+    resetStyleManager: 1,
+
+    // Column padding (this way it's easier select columns)
+    columnsPadding: '10px 0',
+  };
+
+  // Load defaults
+  for (let name in defaults) {
+    if (!(name in c))
+      c[name] = defaults[name];
+  }
+
+  let config = editor.getConfig();
+
+  // I need to prevent forced class creation as classes aren't working
+  // at the moment
+  config.forceClass = 0;
+
+  // Don't need to create css rules with media
+  config.devicePreviewMode = 1;
+
+  // Add Blocks
+  require('./blocks').default(editor, c);
+
+  // Add Components
+  require('./components').default(editor, c);
+
+  // Add Commands
+  require('./commands').default(editor, c);
+
+  // Add Buttons
+  require('./buttons').default(editor, c);
+
+  // Extend Style Manager
+  require('./style').default(editor, c);
+
+  // Update devices
+  if (c.resetDevices) {
+    const dm = editor.DeviceManager;
+    dm.getAll().reset();
+    dm.add('Desktop', '');
+    dm.add('Mobile', '320px');
+  }
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -6,32 +6,24 @@ var env = process.env.WEBPACK_ENV;
 var plugins = [];
 
 if(env !== 'dev'){
-  plugins.push(new webpack.optimize.UglifyJsPlugin({ compressor: { warnings: false } }));
+  //plugins.push(new webpack.optimize.UglifyJsPlugin({compressor: { warnings: false }}));
   plugins.push(new webpack.BannerPlugin(pkg.name + ' - ' + pkg.version));
 }
 
 module.exports = {
-  entry: './src/main',
+  entry: './src',
   output: {
       filename: './dist/' + name + '.min.js',
       library: name,
       libraryTarget: 'umd',
   },
   module: {
-    preLoaders: [
-        { test: /\.json$/, loader: 'json'},
-    ],
-    loaders: [
-      {
-        test: /\.jsx?$/,
+    loaders: [{
+        test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
         include: /src/,
-        query: {
-          presets: ['es2015']
-        }
-      },
-    ],
+        exclude: /node_modules/
+    }],
   },
-  plugins: plugins
+  plugins: plugins,
 };
