6a52aeec5c23e616601431d330bb94e37f499b65
Artur Arseniev <artur.catch@hotmail.it>
2017-12-20 14:34:25 +0100

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,39 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var fs = require('fs');
-var webpack = require('webpack');
-var pkg = require('./package.json');
-var name = pkg.name;
-var plugins = [];
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const pkg = require('./package.json');
+const webpack = require('webpack');
+const fs = require('fs');
+const name = pkg.name;
+let plugins = [];
 
-if (process.env.WEBPACK_ENV !== 'dev') {
-  //plugins.push(new webpack.optimize.UglifyJsPlugin({compressor: { warnings: false }}));
-  plugins.push(new webpack.BannerPlugin(name + ' - ' + pkg.version));
-} else {
-  var index = 'index.html';
-  var indexDev = '_' + index;
-  plugins.push(new HtmlWebpackPlugin({
-    template: fs.existsSync(indexDev) ? indexDev : index
-  }));
+module.exports = (env = {}) => {
+  if (env.production) {
+    plugins = [
+      new webpack.optimize.UglifyJsPlugin({ minimize: true, compressor: { warnings: false }}),
+      new webpack.BannerPlugin(`${name} - ${pkg.version}`),
+    ]
+  } else {
+    const index = 'index.html';
+    const indexDev = '_' + index;
+    plugins.push(new HtmlWebpackPlugin({
+      template: fs.existsSync(indexDev) ? indexDev : index
+    }));
+  }
+
+  return {
+    entry: './src',
+    output: {
+        filename: `./dist/${name}.min.js`,
+        library: name,
+        libraryTarget: 'umd',
+    },
+    module: {
+      loaders: [{
+          test: /\.js$/,
+          loader: 'babel-loader',
+          include: /src/,
+      }],
+    },
+    externals: {'grapesjs': 'grapesjs'},
+    plugins: plugins,
+  };
 }
-
-module.exports = {
-  entry: './src',
-  output: {
-      filename: './dist/' + name + '.min.js',
-      library: name,
-      libraryTarget: 'umd',
-  },
-  module: {
-    loaders: [{
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: /src/,
-        exclude: /node_modules/
-    }],
-  },
-  externals: {'grapesjs': 'grapesjs'},
-  plugins: plugins,
-};
