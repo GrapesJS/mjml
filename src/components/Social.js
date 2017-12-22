1020a2a72796dacb391ba333e601f87a2c500e1b
Artur Arseniev <artur.catch@hotmail.it>
2017-12-22 16:45:29 +0100

    Refactor `mj-social` component

---
+++ src/components/Social.js
@@ -0,0 +1,137 @@
+// Specs: https://mjml.io/documentation/#mjml-social
+
+export default (editor, {
+  dc, opt, defaultModel, defaultView, coreMjmlModel, coreMjmlView
+}) => {
+  const type = 'mj-social';
+
+  dc.addType(type, {
+
+
+    model: defaultModel.extend({ ...coreMjmlModel,
+
+      defaults: { ...defaultModel.prototype.defaults,
+        'custom-name': 'Social',
+        draggable: '[data-type=mj-column]',
+        droppable: false,
+        stylable: [
+          'text-decoration', 'align', 'font-family', 'font-size', 'line-height',
+          'padding', 'padding-top', 'padding-left', 'padding-right', 'padding-bottom',
+          'border-radius', 'border-top-left-radius', 'border-top-right-radius', 'border-bottom-left-radius', 'border-bottom-right-radius',
+          'container-background-color',
+          'color',
+          'facebook-icon-color', 'twitter-icon-color', 'google-icon-color',
+          'instagram-icon-color', 'linkedin-icon-color', 'pinterest-icon-color',
+        ],
+        style: {
+          'align': 'center',
+          'icon-size': '20px',
+          'font-size': '13px',
+          'line-height': '22px',
+          'facebook-icon-color': '#3b5998',
+          'twitter-icon-color': '#55acee',
+          'google-icon-color': '#dc4e41',
+          'instagram-icon-color': '#3f729b',
+          'linkedin-icon-color': '#0077b5',
+          'pinterest-icon-color': '#bd081c',
+          'display': 'facebook twitter google'
+        },
+        traits: [
+          'display', // facebook twitter google
+          {
+            name: 'facebook-content',
+            value: 'Share',
+          },{
+            name: 'facebook-href',
+            value: '[[SHORT_PERMALINK]]',
+          },{
+            name: 'twitter-content',
+            value: 'Tweet',
+          },{
+            name: 'twitter-href',
+            value: '[[SHORT_PERMALINK]]',
+          },{
+            name: 'google-content',
+            value: '+1',
+          },{
+            name: 'google-href',
+            value: '[[SHORT_PERMALINK]]',
+          },{
+            name: 'instagram-content',
+            value: 'Share',
+          },{
+            name: 'instagram-href',
+            value: '[[SHORT_PERMALINK]]',
+          },{
+            name: 'linkedin-content',
+            value: 'Share',
+          },{
+            name: 'linkedin-href',
+            value: '[[SHORT_PERMALINK]]',
+          },{
+            name: 'pinterest-content',
+            value: 'Pin it',
+          },{
+            name: 'pinterest-href',
+            value: '[[SHORT_PERMALINK]]',
+          },{
+            type: 'select',
+            label: 'Mode',
+            name: 'mode',
+            options: [
+              {value: 'horizontal', name: 'Horizontal'},
+              {value: 'vertical', name: 'Vertical'},
+            ]
+          }
+        ],
+        void: true,
+      },
+    },{
+
+      isComponent(el) {
+        if (el.tagName == type.toUpperCase()) {
+          return { type };
+        }
+      },
+    }),
+
+
+    view: defaultView.extend({ ...coreMjmlView,
+
+      tagName: 'tr',
+
+      attributes: {
+        style: 'pointer-events: all; display: table; width: 100%',
+      },
+
+      getMjmlTemplate() {
+        return {
+          start: `<mjml><mj-body><mj-column>`,
+          end: `</mj-column></mj-body></mjml>`,
+        };
+      },
+
+      getTemplateFromEl(sandboxEl) {
+        return sandboxEl.querySelector('tr').innerHTML;
+      },
+
+      render() {
+        this.renderAttributes();
+        this.el.innerHTML = this.getTemplateFromMjml();
+        let content = this.model.get('content').trim();
+
+        if (content) {
+          this.getChildrenContainer().innerHTML = content;
+        }
+
+        this.renderChildren();
+        this.el.style = this.attributes.style;
+        return this;
+      },
+
+      getChildrenSelector() {
+        return 'img';
+      }
+    }),
+  });
+}
--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,7 @@ import loadColumn from './Column';
 import loadText from './Text';
 import loadButton from './Button';
 import loadImage from './Image';
+import loadSocial from './Social';
 
 export default (editor, opt = {}) => {
   let domc = editor.DomComponents;
@@ -253,6 +254,7 @@ export default (editor, opt = {}) => {
   loadText(editor, compOpts);
   loadButton(editor, compOpts);
   loadImage(editor, compOpts);
+  loadSocial(editor, compOpts);
 
 
 
@@ -276,138 +278,6 @@ export default (editor, opt = {}) => {
     view: defaultView,
   });
 */
-
-
-
-
-  // Social
-  domc.addType('mj-social', {
-    model: defaultModel.extend(Object.assign({}, coreMjmlModel, {
-      defaults: Object.assign({}, defaultModel.prototype.defaults, {
-        'custom-name': 'Social',
-        draggable: '[data-type=mj-column]',
-        droppable: false,
-        stylable: [
-          'text-decoration', 'align', 'font-family', 'font-size', 'line-height',
-          'padding', 'padding-top', 'padding-left', 'padding-right', 'padding-bottom',
-          'border-radius', 'border-top-left-radius', 'border-top-right-radius', 'border-bottom-left-radius', 'border-bottom-right-radius',
-          'container-background-color',
-          'color',
-          'facebook-icon-color', 'twitter-icon-color', 'google-icon-color',
-          'instagram-icon-color', 'linkedin-icon-color', 'pinterest-icon-color',
-        ],
-        style: {
-          'align': 'center',
-          'icon-size': '20px',
-          'font-size': '13px',
-          'line-height': '22px',
-          'facebook-icon-color': '#3b5998',
-          'twitter-icon-color': '#55acee',
-          'google-icon-color': '#dc4e41',
-          'instagram-icon-color': '#3f729b',
-          'linkedin-icon-color': '#0077b5',
-          'pinterest-icon-color': '#bd081c',
-          'display': 'facebook twitter google'
-        },
-        traits: [
-          'display', // facebook twitter google
-          {
-            name: 'facebook-content',
-            value: 'Share',
-          },{
-            name: 'facebook-href',
-            value: '[[SHORT_PERMALINK]]',
-          },{
-            name: 'twitter-content',
-            value: 'Tweet',
-          },{
-            name: 'twitter-href',
-            value: '[[SHORT_PERMALINK]]',
-          },{
-            name: 'google-content',
-            value: '+1',
-          },{
-            name: 'google-href',
-            value: '[[SHORT_PERMALINK]]',
-          },{
-            name: 'instagram-content',
-            value: 'Share',
-          },{
-            name: 'instagram-href',
-            value: '[[SHORT_PERMALINK]]',
-          },{
-            name: 'linkedin-content',
-            value: 'Share',
-          },{
-            name: 'linkedin-href',
-            value: '[[SHORT_PERMALINK]]',
-          },{
-            name: 'pinterest-content',
-            value: 'Pin it',
-          },{
-            name: 'pinterest-href',
-            value: '[[SHORT_PERMALINK]]',
-          },{
-            type: 'select',
-            label: 'Mode',
-            name: 'mode',
-            options: [
-              {value: 'horizontal', name: 'Horizontal'},
-              {value: 'vertical', name: 'Vertical'},
-            ]
-          }
-        ],
-        void: true,
-      }),
-    }),{
-      isComponent(el) {
-        if (el.tagName == 'MJ-SOCIAL') {
-          return {type: 'mj-social'};
-        }
-      },
-    }),
-
-    view: defaultView.extend(Object.assign({}, coreMjmlView, {
-      tagName: 'tr',
-
-      attributes: {
-        style: 'pointer-events: all; display: table; width: 100%',
-      },
-
-      getMjmlTemplate() {
-        return {
-          start: `<mjml><mj-body><mj-column>`,
-          end: `</mj-column></mj-body></mjml>`,
-        };
-      },
-
-      getTemplateFromEl(sandboxEl) {
-        return sandboxEl.querySelector('tr').innerHTML;
-      },
-
-      render() {
-        this.renderAttributes();
-        this.el.innerHTML = this.getTemplateFromMjml();
-        let content = this.model.get('content').trim();
-
-        if (content) {
-          this.getChildrenContainer().innerHTML = content;
-        }
-
-        this.renderChildren();
-        this.el.style = this.attributes.style;
-        return this;
-      },
-
-      getChildrenSelector() {
-        return 'img';
-      }
-    }))
-
-  });
-
-
-
 
 
   // Divider
