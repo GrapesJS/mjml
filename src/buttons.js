e8764355c937ce44249700407e9385329beacd54
Lars Decker <lars.decker@gmail.com>
2019-07-15 15:44:45 +0200

    Add Undo & Redo Functions

--- src/buttons.js
+++ src/buttons.js
@@ -1,12 +1,82 @@
 export default (editor, opt = {}) => {
 
+    const tltAttr = 'title';
+    const tltPosAttr = 'data-tooltip-pos';
+
+    let updateTooltip = (coll) => {
+        coll.each((item) => {
+            var attrs = item.get('attributes');
+            attrs[tltPosAttr] = 'bottom';
+            item.set('attributes', attrs);
+        });
+    };
+
     let pnm = editor.Panels;
-    let optPanel = pnm.getPanel('options');
+
 
     pnm.addButton('options', {
         id: 'mjml-import',
         className: 'fa fa-download',
         command: 'mjml-import',
     });
+
+    // let optPanel = pnm.getPanel('options');
+    // if (optPanel) {
+    //     // Fix tooltip position
+    //     let cmdBtns = optPanel.get('buttons');
+    //     cmdBtns.each((btn) => {
+    //         const attrs = btn.get('attributes');
+    //         attrs[tltPosAttr] = 'bottom';
+    //         btn.set('attributes', attrs);
+    //     });
+    //     // Remove preview
+    //     let prvBtn = pnm.addButton('options', 'preview');
+    //     prvBtn && cmdBtns.remove(prvBtn);
+    // }
+
+    // Clean commands panel
+    let cmdPanel = pnm.getPanel('options');
+    if(cmdPanel){
+        let cmdBtns = cmdPanel.get('buttons');
+        // cmdBtns.reset();
+        cmdBtns.add([{
+            id: 'undo',
+            className: 'fa fa-undo',
+            command: 'undo',
+            attributes: {[tltAttr]: opt.cmdBtnUndoLabel}
+        }, {
+            id: 'redo',
+            className: 'fa fa-repeat',
+            command: 'redo',
+            attributes: {[tltAttr]: opt.cmdBtnRedoLabel}
+        }]);
+        updateTooltip(cmdBtns);
+    }
+    // Turn off default devices select and create new one
+    editor.getConfig().showDevices = 0;
+    let devicePanel = pnm.addPanel({
+        id: 'devices-c'
+    });
+    let deviceBtns = devicePanel.get('buttons');
+    devicePanel.get('buttons').add([{
+        id: 'deviceDesktop',
+        command: 'set-device-desktop',
+        className: 'fa fa-desktop',
+        attributes: {[tltAttr]: opt.cmdBtnDesktopLabel},
+        active: 1,
+    }, {
+        id: 'deviceTablet',
+        command: 'set-device-tablet',
+        className: 'fa fa-tablet',
+        attributes: {[tltAttr]: opt.cmdBtnTabletLabel},
+    }, {
+        id: 'deviceMobile',
+        command: 'set-device-mobile',
+        className: 'fa fa-mobile',
+        attributes: {[tltAttr]: opt.cmdBtnMobileLabel},
+    }]);
+    updateTooltip(deviceBtns);
+    //let viewPanel = pnm.getPanel('views');
+    //viewPanel && updateTooltip(viewPanel.get('buttons'));
 
 }
--- src/commands.js
+++ src/commands.js
@@ -1,9 +1,43 @@
+import tglImagesCommand from './toggleImagesCommand';
+
 export default (editor, opt = {}) => {
-    let cmd = editor.Commands;
-    let importCommand = require('./command-import-mjml');
-    let exportCommand = require('./command-export-mjml');
-    let exportName = opt.overwriteExport ? 'export-template' : 'mjml-export';
+    const cmd = editor.Commands;
+    const importCommand = require('./command-import-mjml');
+    const exportCommand = require('./command-export-mjml');
+    const exportName = opt.overwriteExport ? 'export-template' : 'mjml-export';
 
     cmd.add('mjml-import', importCommand.default(editor, opt));
     cmd.add(exportName, exportCommand.default(editor, opt));
+
+    cmd.add(opt.cmdTglImages, tglImagesCommand(opt));
+
+    cmd.add('undo', {
+        run(editor, sender) {
+            sender.set('active', 0);
+            editor.UndoManager.undo(1);
+        }
+    });
+
+    cmd.add('redo', {
+        run(editor, sender) {
+            sender.set('active', 0);
+            editor.UndoManager.redo(1);
+        }
+    });
+    cmd.add('set-device-desktop', {
+        run(editor) {
+            editor.setDevice('Desktop');
+        }
+    });
+    cmd.add('set-device-tablet', {
+        run(editor) {
+            editor.setDevice('Tablet');
+        }
+    });
+    cmd.add('set-device-mobile', {
+        run(editor) {
+            editor.setDevice('Mobile portrait');
+        }
+    });
+
 }
