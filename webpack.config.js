c1631f849e5ba5f178826615c29190f374d333dc
Daan Roet <daanroet@gmail.com>
2019-08-12 17:06:13 +0200

    fix: make sure development mode still works as expected

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,62 @@
 const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const fs = require('fs');
 const pkg = require('./package.json');
 const name = pkg.name;
+let plugins = [];
 
-module.exports = {
-  entry: './src',
-  mode: "development",
-  output: {
-    filename: `./${name}.min.js`,
-    library: name,
-    libraryTarget: 'umd',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+module.exports = (env, options) => {
+  const isProd = (options.mode === 'production');
+  if (!isProd) {
+    const index = 'index.html';
+    const indexDev = `_${index}`;
+    const template = fs.existsSync(indexDev) ? indexDev : index;
+    plugins.push(new HtmlWebpackPlugin({ template }));
+  }
+
+  return {
+    entry: './src',
+    mode: isProd ? 'production' : 'development',
+    output: {
+      path: path.join(__dirname),
+      filename: `dist/${name}.min.js`,
+      library: name,
+      libraryExport: 'default',
+      libraryTarget: 'umd',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
         }
+      ]
+    },
+    resolve: {
+      alias: {
+        'fs': path.resolve(__dirname, 'mocks/fs'),
+        'uglify-js': path.resolve(__dirname, 'mocks/uglify-js'),
       }
-    ]
-  },
-  resolve: {
-    alias: {
-      'fs': path.resolve(__dirname, 'mocks/fs'),
-      'uglify-js': path.resolve(__dirname, 'mocks/uglify-js'),
-    }
-  },
-  // make sure to keep_fnames or else it will break grapesjs, see https://github.com/artf/grapesjs-mjml/issues/110
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          keep_fnames: true
-        },
-      }),
-    ],
-  },
-  target: "web",
-  externals: { 'grapesjs': 'grapesjs' }
+    },
+    // make sure to keep_fnames or else it will break grapesjs, see https://github.com/artf/grapesjs-mjml/issues/110
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            keep_fnames: true
+          },
+        }),
+      ],
+    },
+    target: "web",
+    externals: {'grapesjs': 'grapesjs'},
+    plugins: plugins
+  };
 };
