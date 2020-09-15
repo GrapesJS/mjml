2a5e2696b338310d46cce36fb16f5c1a6fadc16e
Tobias Dittmann <hello@ditsche.dev>
2020-09-15 16:04:31 +0200

    Update de.js

--- src/locale/de.js
+++ src/locale/de.js
@@ -1,1 +1,45 @@
-
+export default {
+  'grapesjs-mjml': {
+    category: '',
+    panels: {
+      buttons: {
+        undo: 'Rückgängig',
+        redo: 'Wiederholen',
+        desktop: 'Desktop',
+        tablet: 'Tablet',
+        mobile: 'Mobile',
+        import: 'MJML importieren',
+      },
+      import: {
+        title: 'MJML importieren',
+        button: 'Importieren',
+        label: '',
+      },
+      export: {
+        title: 'MJML exportieren',
+      },
+    },
+    components: {
+      names: {
+        body: 'Body',
+        button: 'Button',
+        column: 'Spalte',
+        oneColumn: '1 Spalte',
+        twoColumn: '2 Spalten',
+        threeColumn: '3 Spalten',
+        divider: 'Trennstrich',
+        group: 'Gruppe',
+        hero: 'Hero',
+        image: 'Bild',
+        navBar: 'Navigation',
+        navLink: 'Navigationslink',
+        section: 'Sektion',
+        socialGroup: 'Social-Media Symbolleiste',
+        socialElement: 'Social-Media Symbol',
+        spacer: 'Abstand',
+        text: 'Text',
+        wrapper: 'Wrapper',
+      },
+    }
+  },
+};
