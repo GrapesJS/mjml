606b1556e5e84a375ee0affeb7af94cb767accd0
Thibault Henry <thibault@henry.pro>
2020-10-30 17:45:05 +0100

    Create fr.js

---
+++ src/locale/fr.js
@@ -0,0 +1,45 @@
+export default {
+  'grapesjs-mjml': {
+    category: '',
+    panels: {
+      buttons: {
+        undo: 'Annuler',
+        redo: 'Rétablir',
+        desktop: 'Bureau',
+        tablet: 'Tablette',
+        mobile: 'Mobile',
+        import: 'Import MJML',
+      },
+      import: {
+        title: 'Importer MJML',
+        button: 'Importer',
+        label: '',
+      },
+      export: {
+        title: 'Exporter MJML',
+      },
+    },
+    components: {
+      names: {
+        body: 'Corps',
+        button: 'Boutton',
+        column: 'Colonne',
+        oneColumn: '1 Colonne',
+        twoColumn: '2 Colonnes',
+        threeColumn: '3 Colonnes',
+        divider: 'Séparateur',
+        group: 'Groupe',
+        hero: 'Héro',
+        image: 'Image',
+        navBar: 'Bar de navigation',
+        navLink: 'Lien - Bar de navitation',
+        section: 'Section',
+        socialGroup: 'Groupe liens réseaux sociaux',
+        socialElement: 'Lien réseau social',
+        spacer: 'Espace',
+        text: 'Texte',
+        wrapper: 'Conteneur',
+      },
+    }
+  },
+};
