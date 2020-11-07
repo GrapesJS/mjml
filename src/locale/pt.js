30d3825c91dd3360528e3611b7108d45628a3ddf
Samuel Koch <samuel.souza.koch@gmail.com>
2020-11-07 09:36:48 -0300

    Adds pt translation

---
+++ src/locale/pt.js
@@ -0,0 +1,45 @@
+export default {
+  'grapesjs-mjml': {
+    category: '',
+    panels: {
+      buttons: {
+        undo: 'Desfazer',
+        redo: 'Refazer',
+        desktop: 'Desktop',
+        tablet: 'Tablet',
+        mobile: 'Mobile',
+        import: 'Importar MJML',
+      },
+      import: {
+        title: 'Importar MJML',
+        button: 'Importar',
+        label: '',
+      },
+      export: {
+        title: 'Exportar MJML',
+      },
+    },
+    components: {
+      names: {
+        body: 'Body',
+        button: 'Botão',
+        column: 'Coluna',
+        oneColumn: '1 Coluna',
+        twoColumn: '2 Colunas',
+        threeColumn: '3 Colunas',
+        divider: 'Separador',
+        group: 'Grupo',
+        hero: 'Hero',
+        image: 'Imagem',
+        navBar: 'Navegação',
+        navLink: 'Link de navegação',
+        section: 'Seção',
+        socialGroup: 'Mídias sociais',
+        socialElement: 'Mídia social',
+        spacer: 'Espaçamento',
+        text: 'Texto',
+        wrapper: 'Wrapper',
+      },
+    }
+  },
+};
