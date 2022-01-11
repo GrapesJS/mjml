c8d0f57f3704a76e01c4bc6cde8bbac38913f904
Adrián Marraco Bescós <adrian.marraco@gmail.com>
2022-01-11 16:01:51 +0100

    Add Catalan (ca.js) and Spanish (es,js) i18n files
    Add Catalan (ca.js) and Spanish (es,js) i18n files.

---
+++ src/locale/ca.js
@@ -0,0 +1,45 @@
+export default {
+  'grapesjs-mjml': {
+    category: '',
+    panels: {
+      buttons: {
+        undo: 'Desfer',
+        redo: 'Refer',
+        desktop: 'Escriptori',
+        tablet: 'Tauleta',
+        mobile: 'Mòbil',
+        import: 'Importar MJML',
+      },
+      import: {
+        title: 'Importar MJML',
+        button: 'Importar',
+        label: 'Etiqueta',
+      },
+      export: {
+        title: 'Exportar MJML',
+      },
+    },
+    components: {
+      names: {
+        body: 'Cos',
+        button: 'Botó',
+        column: 'Columna',
+        oneColumn: '1 Columna',
+        twoColumn: '2 Columnes',
+        threeColumn: '3 Columnes',
+        divider: 'Divisor',
+        group: 'Grup',
+        hero: 'Hero',
+        image: 'Imatge',
+        navBar: 'Barra navegació',
+        navLink: 'Enllaç barra navegació',
+        section: 'Secció',
+        socialGroup: 'Grup Social',
+        socialElement: 'Element Social',
+        spacer: 'Separador',
+        text: 'Text',
+        wrapper: 'Embolcall',
+      },
+    }
+  },
+};
---
+++ src/locale/es.js
@@ -0,0 +1,45 @@
+export default {
+  'grapesjs-mjml': {
+    category: '',
+    panels: {
+      buttons: {
+        undo: 'Deshacer',
+        redo: 'Rehacer',
+        desktop: 'Escritorio',
+        tablet: 'Tableta',
+        mobile: 'Móvil',
+        import: 'Importar MJML',
+      },
+      import: {
+        title: 'Importar MJML',
+        button: 'Importar',
+        label: 'Etiqueta',
+      },
+      export: {
+        title: 'Exportar MJML',
+      },
+    },
+    components: {
+      names: {
+        body: 'Cuerpo',
+        button: 'Botón',
+        column: 'Columna',
+        oneColumn: '1 Columna',
+        twoColumn: '2 Columnas',
+        threeColumn: '3 Columnas',
+        divider: 'Divisor',
+        group: 'Grupo',
+        hero: 'Hero',
+        image: 'Imagen',
+        navBar: 'Barra navegación',
+        navLink: 'Enlace barra navegación',
+        section: 'Sección',
+        socialGroup: 'Grupo Social',
+        socialElement: 'Elemento Social',
+        spacer: 'Separador',
+        text: 'Texto',
+        wrapper: 'Envoltura',
+      },
+    }
+  },
+};
