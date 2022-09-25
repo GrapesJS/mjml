698139451e9b7351dabd9b0657ddb5efacd7dad1
Artur Arseniev <artur.catch@hotmail.it>
2022-09-25 23:18:47 +0400

    Up mjml

--- src/components/mjml.js
+++ src/components/mjml.js
@@ -1,4 +1,4 @@
-// Specs: https://mjml.io/documentation/#mjml
+// Specs: https://documentation.mjml.io/#mjml
 import { isComponentType } from './utils.js';
 
 export default (editor, { dc, coreMjmlModel, coreMjmlView }) => {
@@ -11,6 +11,21 @@ export default (editor, { dc, coreMjmlModel, coreMjmlView }) => {
       defaults: {
         droppable: '[data-gjs-type=mj-head], [data-gjs-type=mj-body]',
         draggable: false,
+        stylable: false,
+        traits: [
+          {
+            name: 'owa',
+            placeholder: 'eg. desktop',
+          },
+          {
+            name: 'lang',
+            placeholder: 'eg. en',
+          },
+          {
+            name: 'dir',
+            placeholder: 'eg. rtl',
+          },
+        ],
       },
     },
     view: {
