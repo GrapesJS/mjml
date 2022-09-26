b908a68abad1b8beaeb6edff11d687b581fedfd7
Artur Arseniev <artur.catch@hotmail.it>
2022-09-26 13:26:17 +0400

    Update mj-raw block

--- src/blocks.ts
+++ src/blocks.ts
@@ -171,8 +171,11 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
     </mj-wrapper>`,
   });
 
-  bm.add('mj-raw', {
+  addBlock('mj-raw', {
     label: editor.I18n.t('grapesjs-mjml.components.names.raw'),
+    media: `<svg viewBox="0 0 24 24">
+        <path fill="currentColor" d="M12,17.56L16.07,16.43L16.62,10.33H9.38L9.2,8.3H16.8L17,6.31H7L7.56,12.32H14.45L14.22,14.9L12,15.5L9.78,14.9L9.64,13.24H7.64L7.93,16.43L12,17.56M4.07,3H19.93L18.5,19.2L12,21L5.5,19.2L4.07,3Z" />
+    </svg>`,
     content: `<mj-raw>
       <div class="container">
         <img class="item" src="https://source.unsplash.com/random/200x141" alt="Example image">
@@ -183,8 +186,6 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
         <img class="item" src="https://source.unsplash.com/random/200x146" alt="Example image">
       </div>
     </mj-raw>`,
-    attributes: { class: 'fa fa-html5' },
-    ...allBlocks,
   });
 
 };
