3dd0873f8f646af68d5835e4b53a4fad9a996ef1
Artur Arseniev <artur.catch@hotmail.it>
2022-09-26 13:30:39 +0400

    Update blocks

--- src/blocks.ts
+++ src/blocks.ts
@@ -2,20 +2,16 @@ import type grapesjs from 'grapesjs';
 import { RequiredPluginOptions } from '.';
 
 export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
-  const bm = editor.BlockManager;
-  const allBlocks = {
-    category: editor.I18n.t('grapesjs-mjml.category'),
-  };
-
+  const { Blocks } = editor;
   const imagePlaceholderSrc = opts.imagePlaceholderSrc || 'https://via.placeholder.com/350x250/78c5d6/fff';
   const socialIcon = `<svg viewBox="0 0 24 24">
     <path fill="currentColor" d="M18,16.08C17.24,16.08 16.56,16.38 16.04,16.85L8.91,12.7C8.96,12.47 9,12.24 9,12C9,11.76 8.96,11.53 8.91,11.3L15.96,7.19C16.5,7.69 17.21,8 18,8A3,3 0 0,0 21,5A3,3 0 0,0 18,2A3,3 0 0,0 15,5C15,5.24 15.04,5.47 15.09,5.7L8.04,9.81C7.5,9.31 6.79,9 6,9A3,3 0 0,0 3,12A3,3 0 0,0 6,15C6.79,15 7.5,14.69 8.04,14.19L15.16,18.34C15.11,18.55 15.08,18.77 15.08,19C15.08,20.61 16.39,21.91 18,21.91C19.61,21.91 20.92,20.61 20.92,19A2.92,2.92 0 0,0 18,16.08Z" />
   </svg>`;
 
-  opts.resetBlocks && bm.getAll().reset();
+  opts.resetBlocks && Blocks.getAll().reset();
 
   const addBlock = (id: string, def: grapesjs.BlockOptions) => {
-    opts.blocks.indexOf(id)! >= 0 && editor.Blocks.add(id, {
+    opts.blocks.indexOf(id)! >= 0 && Blocks.add(id, {
       select: true,
       category: editor.I18n.t('grapesjs-mjml.category'),
       ...def,
@@ -23,8 +19,10 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
     });
   };
 
+  const getI18nLabel = (label: string) => editor.I18n.t(`grapesjs-mjml.components.names.${label}`)
+
   addBlock('mj-1-column', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.oneColumn'),
+    label: getI18nLabel('oneColumn'),
     media: `<svg viewBox="0 0 24 24">
       <path fill="currentColor" d="M2 20h20V4H2v16Zm-1 0V4a1 1 0 0 1 1-1h20a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1Z"/>
     </svg>`,
@@ -34,7 +32,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-2-columns', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.twoColumn'),
+    label: getI18nLabel('twoColumn'),
     media: `<svg viewBox="0 0 23 24">
       <path fill="currentColor" d="M2 20h8V4H2v16Zm-1 0V4a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1ZM13 20h8V4h-8v16Zm-1 0V4a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1Z"/>
     </svg>`,
@@ -45,7 +43,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-3-columns', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.threeColumn'),
+    label: getI18nLabel('threeColumn'),
     media: `<svg viewBox="0 0 23 24">
       <path fill="currentColor" d="M2 20h4V4H2v16Zm-1 0V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1ZM17 20h4V4h-4v16Zm-1 0V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1ZM9.5 20h4V4h-4v16Zm-1 0V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1Z"/>
     </svg>`,
@@ -57,7 +55,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-text', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.text'),
+    label: getI18nLabel('text'),
     media: `<svg viewBox="0 0 24 24">
       <path fill="currentColor" d="M18.5,4L19.66,8.35L18.7,8.61C18.25,7.74 17.79,6.87 17.26,6.43C16.73,6 16.11,6 15.5,6H13V16.5C13,17 13,17.5 13.33,17.75C13.67,18 14.33,18 15,18V19H9V18C9.67,18 10.33,18 10.67,17.75C11,17.5 11,17 11,16.5V6H8.5C7.89,6 7.27,6 6.74,6.43C6.21,6.87 5.75,7.74 5.3,8.61L4.34,8.35L5.5,4H18.5Z" />
     </svg>`,
@@ -66,7 +64,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-button', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.button'),
+    label: getI18nLabel('button'),
     media: `<svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M20 20.5C20 21.3 19.3 22 18.5 22H13C12.6 22 12.3 21.9 12 21.6L8 17.4L8.7 16.6C8.9 16.4 9.2 16.3 9.5 16.3H9.7L12 18V9C12 8.4 12.4 8 13 8S14 8.4 14 9V13.5L15.2 13.6L19.1 15.8C19.6 16 20 16.6 20 17.1V20.5M20 2H4C2.9 2 2 2.9 2 4V12C2 13.1 2.9 14 4 14H8V12H4V4H20V12H18V14H20C21.1 14 22 13.1 22 12V4C22 2.9 21.1 2 20 2Z" />
     </svg>`,
@@ -74,7 +72,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-image', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.image'),
+    label: getI18nLabel('image'),
     media: `<svg viewBox="0 0 24 24">
       <path fill="currentColor" d="M21,3H3C2,3 1,4 1,5V19A2,2 0 0,0 3,21H21C22,21 23,20 23,19V5C23,4 22,3 21,3M5,17L8.5,12.5L11,15.5L14.5,11L19,17H5Z" />
     </svg>`,
@@ -83,7 +81,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-divider', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.divider'),
+    label: getI18nLabel('divider'),
     media: `<svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M21 18H2V20H21V18M19 10V14H4V10H19M20 8H3C2.45 8 2 8.45 2 9V15C2 15.55 2.45 16 3 16H20C20.55 16 21 15.55 21 15V9C21 8.45 20.55 8 20 8M21 4H2V6H21V4Z" />
     </svg>`,
@@ -91,7 +89,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-social-group', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.socialGroup'),
+    label: getI18nLabel('socialGroup'),
     media: socialIcon,
     content: `<mj-social font-size="12px" icon-size="24px" border-radius="12px" mode="horizontal">
         <mj-social-element name="facebook"></mj-social-element>
@@ -101,13 +99,13 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-social-element', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.socialElement'),
+    label: getI18nLabel('socialElement'),
     media: socialIcon,
     content: '<mj-social-element name="facebook" />',
   });
 
   addBlock('mj-spacer', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.spacer'),
+    label: getI18nLabel('spacer'),
     media: `<svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M8,18H11V15H2V13H22V15H13V18H16L12,22L8,18M12,2L8,6H11V9H2V11H22V9H13V6H16L12,2Z" />
     </svg>`,
@@ -115,7 +113,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-navbar', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.navBar'),
+    label: getI18nLabel('navBar'),
     media: `<svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
     </svg>`,
@@ -128,7 +126,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-navbar-link', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.navLink'),
+    label: getI18nLabel('navLink'),
     media: `<svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M21,15.61L19.59,17L14.58,12L19.59,7L21,8.39L17.44,12L21,15.61M3,6H16V8H3V6M3,13V11H13V13H3M3,18V16H16V18H3Z" />
     </svg>`,
@@ -136,7 +134,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-hero', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.hero'),
+    label: getI18nLabel('hero'),
     media: `<svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M20,20H4A2,2 0 0,1 2,18V6A2,2 0 0,1 4,4H20A2,2 0 0,1 22,6V18A2,2 0 0,1 20,20M4,6V18H20V6H4M6,9H18V11H6V9M6,13H16V15H6V13Z" />
     </svg>`,
@@ -151,7 +149,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-wrapper', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.wrapper'),
+    label: getI18nLabel('wrapper'),
     media: `<svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M18 2H6C4.89 2 4 2.9 4 4V20C4 21.11 4.89 22 6 22H18C19.11 22 20 21.11 20 20V4C20 2.9 19.11 2 18 2M18 20H6V16H18V20M18 8H6V4H18V8Z" />
     </svg>`,
@@ -172,7 +170,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   });
 
   addBlock('mj-raw', {
-    label: editor.I18n.t('grapesjs-mjml.components.names.raw'),
+    label: getI18nLabel('raw'),
     media: `<svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M12,17.56L16.07,16.43L16.62,10.33H9.38L9.2,8.3H16.8L17,6.31H7L7.56,12.32H14.45L14.22,14.9L12,15.5L9.78,14.9L9.64,13.24H7.64L7.93,16.43L12,17.56M4.07,3H19.93L18.5,19.2L12,21L5.5,19.2L4.07,3Z" />
     </svg>`,
