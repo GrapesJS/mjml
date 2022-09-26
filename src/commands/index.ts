2440c6ba0ebb7921d808b35160a2d2746f69d5c4
Artur Arseniev <artur.catch@hotmail.it>
2022-09-26 15:48:43 +0400

    Update device commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,33 +3,36 @@ import { RequiredPluginOptions } from '..';
 import importCommand from './command-import-mjml';
 import exportCommand from './command-export-mjml';
 
-export default (editor: grapesjs.Editor, opt: RequiredPluginOptions) => {
-  const cmd = editor.Commands;
-  const exportName = opt.overwriteExport ? 'export-template' : 'mjml-export';
+export const cmdDeviceDesktop = 'set-device-desktop';
+export const cmdDeviceTablet = 'set-device-tablet';
+export const cmdDeviceMobile = 'set-device-mobile';
+export const cmdImportMjml = 'mjml-import';
 
-  cmd.add('mjml-import', importCommand(editor, opt));
-  cmd.add('mjml-import:change', {
+export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
+  const { Commands } = editor;
+  const exportName = opts.overwriteExport ? 'export-template' : 'mjml-export';
+
+  Commands.add('mjml-import', importCommand(editor, opts));
+  Commands.add('mjml-import:change', {
     run() {
       const code = editor.getHtml();
       return code.trim();
     }
   });
-  cmd.add(exportName, exportCommand(editor, opt));
+  Commands.add(exportName, exportCommand(editor, opts));
 
-  cmd.add('set-device-desktop', {
-    run(editor) {
-      editor.setDevice('Desktop');
-    }
+  // Device commands
+  Commands.add(cmdDeviceDesktop, {
+    run: ed => ed.setDevice('Desktop'),
+    stop: () => {},
   });
-  cmd.add('set-device-tablet', {
-    run(editor) {
-      editor.setDevice('Tablet');
-    }
+  Commands.add(cmdDeviceTablet, {
+    run: ed => ed.setDevice('Tablet'),
+    stop: () => {},
   });
-  cmd.add('set-device-mobile', {
-    run(editor) {
-      editor.setDevice('Mobile portrait');
-    }
+  Commands.add(cmdDeviceMobile, {
+    run: ed => ed.setDevice('Mobile portrait'),
+    stop: () => {},
   });
 
 };
