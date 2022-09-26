b1614e119cbb7c1c7f78a418f89d4e02cec95865
Artur Arseniev <artur.catch@hotmail.it>
2022-09-26 11:37:14 +0400

    Add TS PluginOptions

---
+++ src/index.ts
@@ -0,0 +1,157 @@
+import type grapesjs from 'grapesjs';
+import loadBlocks from './blocks';
+import loadComponents from './components';
+import loadCommands from './commands';
+import loadButtons from './buttons';
+import loadStyle from './style';
+import en from './locale/en';
+
+export type PluginOptions = {
+  /**
+   * Which blocks to add.
+   * @default (all)
+   */
+  blocks?: string[];
+
+  /**
+   * Default MJML template, in case the canvas is empty.
+   * @default ''
+   */
+  defaultTemplate?: string;
+
+  /**
+   * Code viewer theme.
+   * @default 'hopscotch'
+   */
+  codeViewerTheme?: string;
+
+  /**
+   * Placeholder MJML template for the import modal
+   * @default ''
+   */
+  importPlaceholder?: string;
+
+  /**
+   * Image placeholder source for mj-image block
+   * @default ''
+   */
+  imagePlaceholderSrc?: string;
+
+  /**
+   * Overwrite default export command
+   * @default true
+   */
+  overwriteExport?: boolean;
+
+  /**
+   * String before the MJML in export code
+   * @default ''
+   */
+  preMjml?: string;
+
+  /**
+   * String after the MJML in export code
+   * @default ''
+   */
+  postMjml?: string;
+
+  /**
+   * Clean all previous blocks if true
+   * @default true
+   */
+  resetBlocks?: boolean;
+
+  /**
+   * Reset the Style Manager and add new properties for MJML
+   * @default true
+   */
+  resetStyleManager?: boolean;
+
+  /**
+   * Column padding (this way it's easier to select columns)
+   * @default '10px 0'
+   */
+  columnsPadding?: string;
+
+  /**
+   * I18n object containing languages, [more info](https://grapesjs.com/docs/modules/I18n.html#configuration).
+   * @default {}
+   */
+  i18n?: Record<string, any>;
+
+  /**
+   * Custom fonts on exported HTML header, [more info](https://github.com/mjmlio/mjml#inside-nodejs).
+   * @default {}
+   * @example
+   * {
+   *   Montserrat: 'https://fonts.googleapis.com/css?family=Montserrat',
+   *   'Open Sans': 'https://fonts.googleapis.com/css?family=Open+Sans'
+   * }
+   */
+  fonts?: Record<string, any>;
+};
+
+export type RequiredPluginOptions = Required<PluginOptions>;
+
+const plugin: grapesjs.Plugin<PluginOptions> = (editor, opt = {}) => {
+  const opts: RequiredPluginOptions = {
+    defaultTemplate: '',
+    codeViewerTheme: 'hopscotch',
+    importPlaceholder: '',
+    imagePlaceholderSrc: '',
+    overwriteExport: true,
+    preMjml: '',
+    postMjml: '',
+    resetBlocks: true,
+    resetStyleManager: true,
+    columnsPadding: '10px 0',
+    i18n: {},
+    fonts: {},
+    // Export 'mjml', 'html' or both (leave empty) TODO
+    // exportOnly: '',
+    ...opt,
+  };
+
+  const config = editor.getConfig();
+
+  // I need to prevent forced class creation as classes aren't working
+  // at the moment
+  config.forceClass = 0;
+
+  // Don't need to create css rules with media
+  config.devicePreviewMode = 1;
+
+  // Doesn't work without inline styling
+  config.avoidInlineStyle = 0;
+
+  // Load i18n files
+  editor.I18n.addMessages({
+    en,
+    ...opts.i18n,
+  });
+
+  [
+    loadBlocks,
+    loadComponents,
+    loadCommands,
+    loadButtons,
+    loadStyle,
+  ].forEach(module => module(editor, opts));
+
+  // Update devices
+  if (opts.resetDevices) {
+    const dm = editor.DeviceManager;
+    dm.getAll().reset();
+    dm.add('Desktop', '');
+    dm.add('Mobile', '320px');
+    dm.add('Tablet', '820px');
+  }
+};
+
+export default plugin;
+
+export const blocksPlugin = masterPlugin.blocksPlugin = loadBlocks;
+export const componentsPlugin = masterPlugin.componentsPlugin = loadComponents;
+export const commandsPlugin = masterPlugin.commandsPlugin =  loadCommands;
+export const buttonsPlugin = masterPlugin.buttonsPlugin = loadButtons;
+export const stylePlugin = masterPlugin.stylePlugin = loadStyle;
