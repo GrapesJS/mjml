a4a96f5c5f0b8b9695b2461691b246ce569fc744
Artur Arseniev <artur.catch@hotmail.it>
2022-09-26 12:46:30 +0400

    Update mj-image block

--- src/blocks.ts
+++ src/blocks.ts
@@ -70,11 +70,13 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
     content: '<mj-button>Button</mj-button>',
   });
 
-  bm.add('mj-image', {
+  addBlock('mj-image', {
     label: editor.I18n.t('grapesjs-mjml.components.names.image'),
-    content: '<mj-image src="' + imagePlaceholderSrc + '"/>',
-    attributes: { class: 'fa fa-image' },
-    ...allBlocks,
+    media: `<svg viewBox="0 0 24 24">
+      <path fill="currentColor" d="M21,3H3C2,3 1,4 1,5V19A2,2 0 0,0 3,21H21C22,21 23,20 23,19V5C23,4 22,3 21,3M5,17L8.5,12.5L11,15.5L14.5,11L19,17H5Z" />
+    </svg>`,
+    content: `<mj-image src="${imagePlaceholderSrc}"/>`,
+    activate: true,
   });
 
   bm.add('mj-divider', {
