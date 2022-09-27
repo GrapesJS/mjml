ae97a0f70692501698d219d764ea2184be59eec1
Artur Arseniev <artur.catch@hotmail.it>
2022-09-27 16:31:03 +0400

    Clean open import MJML

--- src/commands/openImportMjml.ts
+++ src/commands/openImportMjml.ts
@@ -3,35 +3,11 @@ import { RequiredPluginOptions } from '..';
 
 export default (editor: grapesjs.Editor, opts: RequiredPluginOptions, cmdId: string) => {
   const config = editor.getConfig();
-  // @ts-ignore
-  // const codeViewer = editor.CodeManager.getViewer('CodeMirror').clone();
-  // const btnImp = document.createElement('button');
-  // const container = document.createElement('div');
   const pfx = config.stylePrefix || '';
 
-  // Init import button
-  // btnImp.innerHTML = editor.I18n.t('grapesjs-mjml.panels.import.button');
-  // btnImp.className = `${pfx}btn-prim ${pfx}btn-import`;
-  // btnImp.onclick = () => {
-  //   const code = codeViewer.editor.getValue();
-  //   editor.DomComponents.getWrapper().set('content', '');
-  //   editor.setComponents(code.trim());
-  //   editor.Modal.close();
-  //   editor.runCommand('mjml-import:change');
-  // };
+  const getI18nLabel = (label: string) => editor.I18n.t(`grapesjs-mjml.panels.import.${label}`);
 
-  // Init code viewer
-  // codeViewer.set({
-  //   codeName: 'htmlmixed',
-  //   theme: opts.codeViewerTheme,
-  //   readOnly: 0
-  // });
-
-  const { Commands } = editor;
-
-  const getI18nLabel = (label: string) => editor.I18n.t(`grapesjs-mjml.panels.import.${label}`)
-
-  Commands.add(cmdId, {
+  editor.Commands.add(cmdId, {
     onImport(code: string) {
       editor.Components.getWrapper().set('content', '');
       editor.setComponents(code.trim());
@@ -88,9 +64,7 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions, cmdId: str
     run(editor, sender = {}) {
       const container = this.getCodeContainer();
       let codeEditorMjml = this.codeEditorMjml as any;
-      // let viewer = codeViewer.editor;
 
-      // NEW
       if (!codeEditorMjml) {
         const result = this.createCodeEditor();
         codeEditorMjml = result.codeEditor;
@@ -103,24 +77,6 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions, cmdId: str
         codeEditorMjml.editor.refresh();
       }
 
-      // Init code viewer if not yet instantiated
-      // if (!viewer) {
-      //   const txtarea = document.createElement('textarea');
-      //   const labelImport = getI18nLabel('label');
-
-      //   if (labelImport) {
-      //     let labelEl = document.createElement('div');
-      //     labelEl.className = pfx + 'import-label';
-      //     labelEl.innerHTML = labelImport;
-      //     container.appendChild(labelEl);
-      //   }
-
-      //   container.appendChild(txtarea);
-      //   container.appendChild(btnImp);
-      //   codeViewer.init(txtarea);
-      //   viewer = codeViewer.editor;
-      // }
-
       editor.Modal.open({
         title: getI18nLabel('title'),
         content: container
@@ -128,9 +84,6 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions, cmdId: str
         sender.set && sender.set('active', false);
         editor.stopCommand(cmdId)
       });
-
-      // codeViewer.setContent(opts.importPlaceholder);
-      // viewer.refresh();
     },
 
     stop(editor) {
