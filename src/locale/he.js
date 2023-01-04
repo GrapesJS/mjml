4b19debf21bc4c3e2ec2e06e044ac5c92a295afc
Oren Hahiashvili <64045740+Oren-Hahiashvili@users.noreply.github.com>
2023-01-04 19:02:55 +0200

    - add Hebrew language translation, added `he.js` (#308)

---
+++ src/locale/he.js
@@ -0,0 +1,46 @@
+export default {
+  'grapesjs-mjml': {
+    category: '',
+    panels: {
+      buttons: {
+        undo: 'ביטול',
+        redo: 'בצע שוב',
+        desktop: 'מחשב שולחני',
+        tablet: 'טאבלט',
+        mobile: 'טלפון נייד',
+        import: 'ייבוא MJML',
+      },
+      import: {
+        title: 'ייבוא MJML',
+        button: 'ייבוא',
+        label: '',
+      },
+      export: {
+        title: 'ייצוא MJML',
+      },
+    },
+    components: {
+      names: {
+        body: 'גוף',
+        button: 'כפתור',
+        column: 'טור',
+        oneColumn: 'טור 1',
+        twoColumn: '2 טורים',
+        threeColumn: '3 תורים',
+        divider: 'מפריד',
+        group: 'קבוצה',
+        hero: 'כותרת',
+        image: 'תמונה',
+        navBar: 'תפריט',
+        navLink: 'לינק תפריט',
+        section: 'אזור',
+        socialGroup: 'קבוצה חברתית',
+        socialElement: 'אלמנט קבוצתי',
+        spacer: 'מרווח',
+        text: 'טקסט',
+        wrapper: 'עוטף',
+        raw: 'שורה',
+      },
+    }
+  },
+};
