14cdb3f8e71e4caae59c39bf0b4e9658d1d1257a
Nicolas Dusart <nicolas@freedelity.be>
2024-06-13 17:48:10 +0200

    support mj-wrapper with background-url and full-width

--- src/components/Wrapper.ts
+++ src/components/Wrapper.ts
@@ -47,7 +47,7 @@ export default (editor: Editor, { coreMjmlModel, coreMjmlView }: any) => {
 
       getChildrenSelector() {
         if(this.model.getAttributes()['full-width']){
-          return 'table > tbody > tr > td > div > table > tbody > tr > td';
+          return 'table > tbody > tr > td > div table > tbody > tr > td';
         }else
           return 'table > tbody > tr > td';
       },
