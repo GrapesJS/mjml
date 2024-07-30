cf592e5956cd8bc69f04acaefc44b1c550977683
danstarns <danielstarns@hotmail.com>
2024-07-29 19:32:43 -0700

    init: add mjml snapshot testing

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleFileExtensions: ["js", "ts"],
+  verbose: true,
+  modulePaths: ["<rootDir>/src"],
+  testMatch: ["<rootDir>/tests/specs/**/*.(t|j)s"],
+  setupFiles: ["<rootDir>/tests/setup.js"],
+};
