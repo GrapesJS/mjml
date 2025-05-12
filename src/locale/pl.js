880a1afc2a8e23a8ff4b4723446a046b096bdb77
Piotr <piotr.malinski@socialwifi.com>
2025-05-12 13:34:40 +0200

    Add Polish translations.

---
+++ src/locale/pl.js
@@ -0,0 +1,46 @@
+export default {
+  'grapesjs-mjml': {
+    category: '',
+    panels: {
+      buttons: {
+        undo: 'Cofnij',
+        redo: 'Ponów',
+        desktop: 'Desktop',
+        tablet: 'Tablet',
+        mobile: 'Mobile',
+        import: 'Import MJML',
+      },
+      import: {
+        title: 'Import MJML',
+        button: 'Importuj',
+        label: '',
+      },
+      export: {
+        title: 'Eksport MJML',
+      },
+    },
+    components: {
+      names: {
+        body: 'Body',
+        button: 'Przycisk',
+        column: 'Kolumna',
+        oneColumn: '1 Kolumna',
+        twoColumn: '2 Kolumny',
+        threeColumn: '3 Kolumny',
+        divider: 'Rozdzielacz',
+        group: 'Grupa',
+        hero: 'Hero',
+        image: 'Grafika',
+        navBar: 'Pasek nawigacyjny',
+        navLink: 'Odnośnik paska nawigacyjnego',
+        section: 'Sekcja',
+        socialGroup: 'Widżet społecznościowy',
+        socialElement: 'Element społecznościowy',
+        spacer: 'Odstęp',
+        text: 'Tekst',
+        wrapper: 'Grupa elementów',
+        raw: 'Surowy kod',
+      },
+    }
+  },
+};
